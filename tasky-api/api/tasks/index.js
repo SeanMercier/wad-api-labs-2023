6caf1591546d673b7df231b4f095d848ff7e52d1
seanm <20094677@mail.wit.ie>
2023-11-28 18:17:28 +0000

    Added routing for Updating and Deleting a Task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     res.json(tasksData);
 });
 
-// Get task details
+// Get task details endpoint
 router.get('/:id', (req, res) => {
     const { id } = req.params
     const task = tasksData.tasks.find(task => task.id === id);
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(task);
 });
 
-//Add a task
+//Add a task endpoint
 router.post('/', (req, res) => {
     const { title, description, deadline, priority, done } = req.body;
     const newTask = {
@@ -32,6 +32,29 @@ router.post('/', (req, res) => {
     tasksData.tasks.push(newTask);
     res.status(201).json(newTask);
     tasksData.total_results++;
+});
+
+//Update an existing task endpoint
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+    if (taskIndex === -1) {
+        return res.status(404).json({ status: 404, message: 'Task not found' });
+    }
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    tasksData.tasks[taskIndex] = updatedTask;
+    res.json(updatedTask);
+});
+
+//Delete a task endpoint
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
+    
+    if (taskIndex === -1) return res.status(404).json({status:404,message:'Task not found'});
+    tasksData.tasks.splice(taskIndex, 1);
+    res.status(204).send();
+    tasksData.total_results--;
 });
 
 export default router;
